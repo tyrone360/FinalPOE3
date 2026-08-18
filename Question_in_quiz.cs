7678e596d05f1ddc996977a4ac8f894d80640b80
Hlulani Tyrone Mthombeni <tshungutyrone@gmail.com>
2026-08-18 18:29:55 +0200

    Fix formatting and comments in Question_in_quiz.cs

--- Question_in_quiz.cs
+++ Question_in_quiz.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 
 namespace demo
-{
+{//start of namespace
     public class Question_in_quiz
-    {
+    {//satrt of classs
         // The text of the quiz question
         public string Text { get; set; }
 
@@ -12,5 +12,5 @@ namespace demo
 
         // A list of wrong answer options for the question
         public List<string> wrongAnswer { get; set; }
-    }
-}
+    }//end of class
+}//start of namespace
